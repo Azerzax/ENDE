ea6ff01e658e927d1339716d49f97f4ce269238f
Imrane <Imrane@DESKTOP-OJVAGVA.mshome.net>
2020-11-17 11:32:20 +0100

    menú 4 opciones implementado

--- menuakGit/src/menuakGit/menuak.java
+++ menuakGit/src/menuakGit/menuak.java
@@ -1,6 +1,6 @@
 package menuakGit;
 import java.util.Scanner;
-
+import javax.swing.JOptionPane;
 
 public class menuak {
 	
@@ -11,16 +11,69 @@ public class menuak {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int x;
-		int y;
+		Scanner entrada=new Scanner(System.in);
 		
-		print("=======Menua======\r\n"
+		Double x;
+		Double y;
+		String aukera;
+		
+		x=Double.parseDouble(JOptionPane.showInputDialog("Idatzi x-ren balioa"));
+		y=Double.parseDouble(JOptionPane.showInputDialog("Idatzi y-ren balioa"));
+		
+		do{
+			
+		
+			print("=======Menua======\r\n"
 				+ "a) x+y \r\n"
 				+ "b) x-y \r\n"
-				+ " c) x*y \r\n"
-				+ " d) x/y");
+				+ "c) x*y \r\n"
+				+ "d) x/y \r\n"
+				+ "e) Irten");
+		
+			aukera=entrada.nextLine();
+		
+			switch(aukera) {
+			
+				case "a":{
+							print("Erantzuna: " + (x+y) + " da");
+						}break;
+				
+				case "b":{
+							print("Erantzuna: " + (x-y) + " da");
+						}break;
+				
+				case "c":{
+							print("Erantzuna: " + (x*y) + " da");
+						}break;
+				
+				case "d":{
+							print("Erantzuna: " + (x/y) + " da");
+						}break;
+				
+			    
+			}
+			
+			if(!aukera.equalsIgnoreCase("a") && !aukera.equalsIgnoreCase("b")
+					&& !aukera.equalsIgnoreCase("d") && !aukera.equalsIgnoreCase("c")
+					&& !aukera.equalsIgnoreCase("e")) {
+					print("Aukera desegokia");
+				}
+				
+		}while(!aukera.equalsIgnoreCase("e"));
+			
 		
 
+		
+		print("Programa amaitu da.");	
+
+		
 	}
 
 }
+		
+		
+
+		
+
+	
+
