680dffe65874cc103334130788f9622fe170e29f
Imrane <Imrane@DESKTOP-OJVAGVA.mshome.net>
2020-11-17 10:18:39 +0100

    Menu 4 opciones

--- menuakGit/src/menuakGit/menuak.java
+++ menuakGit/src/menuakGit/menuak.java
@@ -1,10 +1,24 @@
 package menuakGit;
+import java.util.Scanner;
+
 
 public class menuak {
+	
+	public static void print(String texto) {
+		System.out.println(texto);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		int x;
+		int y;
+		
+		print("=======Menua======\r\n"
+				+ "a) x+y \r\n"
+				+ "b) x-y \r\n"
+				+ " c) x*y \r\n"
+				+ " d) x/y");
 		
 
 	}
